684ba76c71b32c86d89480f9fd3fe7fd44350883
mario.pejic <mariopejic02@gmail.com>
2024-11-05 11:54:34 +0100

    napravljen repo, branch development,konzolna aplikacija

--- Class1.cs
+++ Class1.cs
@@ -5,5 +5,6 @@ public class Class1
 	public Class1()
 	{
         Console.WriteLine("„Pozdrav iz aplikacije od Mario Pejić, broj indeksa: 12345");
+        Console.WriteLine("„Pozdrav iz aplikacije od Mario Pejić");
     }
 }
